56f879dbda97ca1db320d4833041f8f3f59ac068
josemartinbm99 <josemartinbm99@gmail.com>
2025-01-22 18:32:25 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,7 +7,8 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+
+"""class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -23,7 +24,50 @@ class Address(Base):
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    person = relationship(Person)"""
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(250))
+    height = Column(String(250))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    climate = Column(String(250))
+    poblation = Column(String(250))
+    orbital_period = Column(String(250))
+    rotation_period = Column(String(250))
+    diameter = Column(String(250))
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    pilots = Column(String(250))
+    cargo_capacity = Column(String(250))
+    crew = Column(String(250))
+    model = Column(String(250))
+    consumables = Column(String(250))
+    
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    characters = relationship(Characters)
+    vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
+    vehicles = relationship(Vehicles)
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    planets = relationship(Planets)
+    
+
 
     def to_dict(self):
         return {}
